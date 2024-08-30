d5ab398b7833ac3b965cb7878f9716d5efd496e9
GirdhariGarg <girdharigarg20@gmail.com>
2024-08-30 19:17:55 -0400

    Update LoginStepDefinition.java

--- src/main/java/com/tmb/stepdefinition/LoginStepDefinition.java
+++ src/main/java/com/tmb/stepdefinition/LoginStepDefinition.java
@@ -12,7 +12,7 @@ public class LoginStepDefinition {
     LoginPage loginPage;
     public WebDriver driver;
 	@Given("I want to write a step with precondition")
-	public void i_want_to_write_a_step_with_precondition throws MalformedURLException() {
+	public void i_want_to_write_a_step_with_precondition() throws MalformedURLException{
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("some other precondition");
 		loginPage=new LoginPage();
